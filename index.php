f65d664c529d434555859a3c4ce68b3db3bbe8db
Alejandro Faustino <htrb.tino@gmail.com>
2024-11-22 02:44:43 +0800

    "Updated Security for Login"

--- index.php
+++ index.php
@@ -3,6 +3,12 @@ session_start();
 
 require_once 'functions.php'; // Include database connection setup
 
+// Guard: Check if the user is already logged in
+if (isset($_SESSION['user_id'])) {
+    header("Location: admin/dashboard.php"); // Redirect to dashboard if already logged in
+    exit(); // Ensure no further code is executed after redirect
+}
+
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
     // Retrieve form data using the postData function
@@ -30,19 +36,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Login</title>
 </head>
-
 <body class="bg-secondary-subtle">
     <div class="d-flex align-items-center justify-content-center vh-100">
         <div class="col-3">
-
-        <?php if (isset($error_message)): ?>
+            <?php if (isset($error_message)): ?>
                 <div class="alert alert-danger" role="alert">
                     <?php echo htmlspecialchars($error_message); ?>
                 </div>
@@ -69,5 +72,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-
 </html>
